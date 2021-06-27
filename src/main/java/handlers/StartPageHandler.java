d6868b0add259ed086a42ba70a87e2cdd45fa6f2
ana_mrzv <ana.mrzv@mail.ru>
2021-06-27 21:11:10 +0300

    add start page handler class and class for information about user's choice of film

---
+++ src/main/java/handlers/StartPageHandler.java
@@ -0,0 +1,62 @@
+package handlers;
+
+import cache.DataCache;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartPageHandler implements InputMessageHandler{
+    private DataCache userDataCache;
+
+    public StartPageHandler(DataCache userDataCache) {
+        this.userDataCache = userDataCache;
+    }
+
+    @Override
+    public SendMessage handle(Message message) {
+        return processUserInput(message);
+    }
+
+    @Override
+    public BotState getHandlerName() {
+        return BotState.START_PAGE;
+    }
+
+    private SendMessage processUserInput(Message message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(message.getText());
+        sendMessage.setReplyMarkup(getReplyKeyboard());
+        int userId = message.getFrom().getId();
+        userDataCache.setUsersCurrentBotState(userId, BotState.START_PAGE);
+        return sendMessage;
+    }
+
+    private ReplyKeyboardMarkup getReplyKeyboard() {
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+            replyKeyboardMarkup.setSelective(true);
+            replyKeyboardMarkup.setResizeKeyboard(true);
+            replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            KeyboardRow keyboardFirstRow = new KeyboardRow();
+            KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+            keyboardFirstRow.add(new KeyboardButton("Фильмы"));
+            keyboard.add(keyboardFirstRow);
+
+            keyboardSecondRow.add(new KeyboardButton("Информация"));
+            keyboard.add(keyboardSecondRow);
+
+            replyKeyboardMarkup.setKeyboard(keyboard);
+
+            return replyKeyboardMarkup;
+    }
+}
