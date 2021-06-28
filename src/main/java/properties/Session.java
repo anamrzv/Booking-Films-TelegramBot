0667a84b112cc156e6dabd4f5f8922f0b639e61a
ana_mrzv <ana.mrzv@mail.ru>
2021-06-28 14:01:17 +0300

    add processing of session button : bot can show days, when films in rent are going. add class Session handle sessions from DB fix showing only films in rent

---
+++ src/main/java/properties/Session.java
@@ -0,0 +1,21 @@
+package properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@AllArgsConstructor
+public class Session {
+    private int id;
+    private LocalDateTime dateAndTime;
+    private int filmId;
+
+    public String getDay(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String text = dateAndTime.format(formatter);
+        return text;
+    }
+}
